367a311ba279559d7d71e5129d6793f96834a2b8
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-11 10:50:40 -0500

    created splash and registration partials

--- app/app.js
+++ app/app.js
@@ -5,8 +5,11 @@ const app = angular.module('Scoring', ['ngRoute']);
 // const isAuth = (userFactory) => userFactory.isAuthenticated();
 
 app.config(($routeProvider) => {
-	// $routeProvider
-	// .when('/', {})
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/splash.html',
+		controller: 'userCtrl'
+	});
 });
 
 app.run((FBCreds) => firebase.initializeApp(FBCreds));
---
+++ app/factories/user-factory.js
@@ -0,0 +1,15 @@
+'use strict';
+
+app.factory('userFactory', function($q, $http) {
+
+	//register a new user in firebase
+
+
+	//login a user to firebase
+
+	//logout a user from firebase
+
+	//check if a user is authenticated
+
+	//get the current user id
+});
