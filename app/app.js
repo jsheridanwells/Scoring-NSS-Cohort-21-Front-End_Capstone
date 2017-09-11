2741988244e0bd609bb20a846e32dc124082f0a4
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-11 10:35:23 -0500

    firebase credentials loaded

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 const app = angular.module('Scoring', ['ngRoute']);
+
+// const isAuth = (userFactory) => userFactory.isAuthenticated();
+
+app.config(($routeProvider) => {
+	// $routeProvider
+	// .when('/', {})
+});
+
+app.run((FBCreds) => firebase.initializeApp(FBCreds));
