1a657d677aaf5b95fc1e7c3c0b4bbab2f23d67ae
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-13 09:15:49 -0500

    create assessment form populates with class rosters

--- app/controllers/assessment-create-ctrl.js
+++ app/controllers/assessment-create-ctrl.js
@@ -4,11 +4,22 @@ app.controller('assessmentCreateCtrl', function ($scope, $location, userFactory,
 
 	let userId = userFactory.getUserId();
 
+	$scope.classes = [];
+
+	const getClassList = () => {
+		classFactory.getAllClasses(userId)
+			.then(classes => {
+				$scope.classes = classes;
+				console.log("$scope.classes", $scope.classes);
+			})
+			.catch(error => console.log("error from getClassList", error));
+	};
+
 	$scope.newAssessmentObj = {
 		assessmentName: '',
 		totalPoints: '',
 		date: '',
-		students: [],
+		classes: [],
 		uid: userId
 	};
 
@@ -19,5 +30,12 @@ app.controller('assessmentCreateCtrl', function ($scope, $location, userFactory,
 			})
 			.catch(error => console.log("error in addAssessment", error.message));
 	};
+
+	$scope.addClass = (id) => {
+		$scope.newAssessmentObj.classes.push(id);
+		console.log("$scope.newAssessmentObj.classes", $scope.newAssessmentObj.classes);
+	};
+
+	getClassList();
 
 });
