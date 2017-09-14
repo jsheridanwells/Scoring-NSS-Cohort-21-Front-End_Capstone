650a1f4e256b664be4d7e131da28936d2c3335c8
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-14 13:02:01 -0500

    adding scores to assessment object, stuck on update to fb

--- app/controllers/assessment-scoring-ctrl.js
+++ app/controllers/assessment-scoring-ctrl.js
@@ -3,9 +3,9 @@ app.controller('assessmentScoringCtrl', function($scope, $routeParams, userFacto
 	let userId = userFactory.getUserId();
 
 	$scope.assessment = {};
-	$scope.students = [];
 
 	$scope.saveAssessment = (assessment) => {
+		console.log("assessment", assessment);
 		assessmentFactory.updateAssessment($routeParams.assessmentId, assessment)
 			.then(data => console.log("data from saveAssessment", data))
 			.catch(error => console.log("error from saveAssessment", error.message));
@@ -15,13 +15,7 @@ app.controller('assessmentScoringCtrl', function($scope, $routeParams, userFacto
 		assessmentFactory.getSingleAssessment($routeParams.assessmentId)
 			.then(assessmentObj => {
 				$scope.assessment = assessmentObj;
-				for (let i = 0; i < assessmentObj.classes.length; i++) {
-					for (let j = 0; j < assessmentObj.classes[i].students.length; j++) {
-						$scope.students.push(assessmentObj.classes[i].students[j]);
-					}
-				}
 				console.log("scope assessment", $scope.assessment);
-				console.log("scope students", $scope.students);
 			})
 			.catch(error => console.log(error.message));
 	};
