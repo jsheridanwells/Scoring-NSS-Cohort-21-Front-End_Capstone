aae021db204097f4e5d9bdf8df23a9e180a10ffb
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-15 08:22:51 -0500

    refactored and commented class controllers

--- app/controllers/class-list-ctrl.js
+++ app/controllers/class-list-ctrl.js
@@ -2,10 +2,13 @@
 
 app.controller('classListCtrl', function ($scope, userFactory, classFactory) {
 
+	//holds uid of current user
 	let userId = userFactory.getUserId();
 
+	// holds array of all classes associated with current user
 	$scope.classes = [];
 
+	//loads $scope.classes array with all classes associated with current user
 	const showClasses = () => {
 		classFactory.getAllClasses(userId)
 			.then(classList => {
@@ -14,12 +17,14 @@ app.controller('classListCtrl', function ($scope, userFactory, classFactory) {
 			.catch(error => console.log("error from showClasses", error.message));
 	};
 
+	//removes class from classes collection in FB
 	$scope.deleteClass = (classId) => {
 		classFactory.deleteClass(classId)
 			.then(() => showClasses())
 			.catch(error => console.log("error from deleteClass", error.message));
 	};
 
+	//loads list of all classes associated with current user on page load
 	showClasses();
 
 });
