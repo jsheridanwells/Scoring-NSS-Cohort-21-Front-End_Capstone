10d2a74c4b345bbbf3e72b4e64ee6c0d6045024d
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-18 11:00:41 -0500

    positioning chart, fixed duplicating SVG error

--- app/charts/donut-generator.js
+++ app/charts/donut-generator.js
@@ -1,64 +1,33 @@
 'use strict';
 
-// app.service('donutGenerator', function (){
-
-// 	this.createDonutChart = (data, el) => {
-//     		let color = ['#99d6ff', '#99ff99', '#ffff99', '#ff9999'];
-//         let width = 300;
-//         let height = 300;
-//         let min = Math.min(width, height);
-//         let svg = d3.select(el).append('svg');
-//         let pie = d3.layout.pie().sort(null);
-//         let arc = d3.svg.arc()
-//           .outerRadius(min / 2 * 0.9)
-//           // .innerRadius(min / 2 * 0.5);
-//           .innerRadius(10);
-
-//         svg.attr({width: width, height: height});
-//         var g = svg.append('g')
-//           .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-
-//         g.selectAll('path').data(pie(data))
-//           .enter().append('path')
-//             .style('stroke', 'white')
-//             .attr('d', arc)
-//             .attr('fill', function(d, i){ return color[i]; });
-// 	};
-
-// });
-
 app.directive('donutChart', function() {
 
   const link = (scope, el, attr) => {
+    //sets color for each level in data array
+    let color = ['#99d6ff', '#99ff99', '#ffff99', '#ff9999'];
+    //sets width and height of SVG, radii of pie chart relative to these values
+    let width = 350;
+    let height = 350;
+    let min = Math.min(width, height);
+    let svg = d3.select(el[0]).append('svg');
+    let pie = d3.layout.pie().sort(null);
+    let arc = d3.svg.arc()
+      .outerRadius(min / 2 * 0.9)
+      .innerRadius(10);
 
+    svg.attr({width: width, height: height});
+    var g = svg.append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+      //when given an array for scope.data, svg updates with paths for chart
     scope.$watch('data', function(data) {
-      console.log("scope watch firing");
-      let results = scope.data;
-
-      let donut = d3.layout.pie();
-      let arcData = donut(results);
-      let arc = d3.svg.arc().innerRadius(100).outerRadius(160);
-
-      arc.startAngle(function (d) {return d.startAngle;});
-      arc.endAngle(function (d) {return d.endAngle;});
-
-      let color = ['#99d6ff', '#99ff99', '#ffff99', '#ff9999'];
-
-      let svg = d3.select(el[0]).append('svg');
-      svg.attr('height', '200').attr('width', '200');
-
-      svg.append('g').attr('transform', 'translate(200,175)')
-                      .selectAll('path').data(arcData).enter()
-                      .append('path').attr('d', arc)
-                      .style('fill', (d,i) => {return color[i];});
+        g.selectAll('path').data(pie(data))
+            .enter().append('path')
+            .style('stroke', 'white')
+            .attr('d', arc)
+            .attr('fill', function(d, i){ return color[i]; });
 
     }, true);
-
-
-    // scope.$watch('data', function(data) {
-    //   el[0].innerHTML = scope.data;
-    // }, true);
-
   };
 
   return {
