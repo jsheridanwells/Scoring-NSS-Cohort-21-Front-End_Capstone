c1a1b8cc6804afc1be751d6dbd93f78da8b5b09e
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-18 14:20:52 -0500

    refactored donut chart

--- app/charts/donut-generator.js
+++ app/charts/donut-generator.js
@@ -19,17 +19,15 @@ app.directive('donutChart', function() {
     var g = svg.append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-      //when given an array for scope.data, svg updates with paths for chart
+    //when given an array for scope.data, svg updates with paths for chart
     scope.$watch('data', function(data) {
 
       let percentages = data.map(d => d.percentage);
-      console.log("percentages", percentages, arc.centroid([10,100]));
-
-        g.selectAll('path').data(pie(percentages))
-            .enter().append('path')
-            .style('stroke', 'white')
-            .attr('d', arc)
-            .attr('fill', function(d, i){ return color[i]; });
+      g.selectAll('path').data(pie(percentages))
+          .enter().append('path')
+          .style('stroke', 'white')
+          .attr('d', arc)
+          .attr('fill', function(d, i){ return color[i]; });
 
     }, true);
   };
@@ -40,5 +38,3 @@ app.directive('donutChart', function() {
     scope: {data: '='}
   };
 });
-
-// scope: {data: '=chartData'},
