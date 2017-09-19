8a952fa10290d30a11e58196706a0012558f81d8
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-19 14:32:17 -0500

    sorts data object properly

--- app/controllers/view-assessment-level-ctrl.js
+++ app/controllers/view-assessment-level-ctrl.js
@@ -1,25 +1,52 @@
 'use strict';
 
-app.controller('viewAssessmentLevelCtrl', function ($rootScope, $scope, $routeParams, userFactory, assessmentFactory, classFactory, proficiencySort) {
-	//holds uid of current user
-	let userId = userFactory.getUserId();
+app.controller('viewAssessmentLevelCtrl', function (
+	$rootScope,
+	$scope,
+	$routeParams,
+	assessmentFactory,
+	proficiencySort) {
 
 	//object holds information for assessment retrieved via $routeParams
 	$scope.assessment = {};
-	$scope.className = '';
-	$scope.currentProficiency = [];
+	$scope.currentClass = {};
+	$scope.currentStudents = [];
 
 	const getAssessment = () => {
 		console.log("rootScope", $rootScope.currentAssessment);
 		assessmentFactory.getSingleAssessment($rootScope.currentAssessment)
 			.then(assessment => {
 				$scope.assessment = assessment;
-				$scope.proficiency = proficiencySort.sortByLevel(assessment);
+				$scope.currentClass = getCurrentClass(assessment, $routeParams.classId);
+				$scope.currentStudents = getCurrentStudents($scope.currentClass, $routeParams.levelName);
+
+
 
 				console.log("scope assessment", $scope.assessment);
-				console.log("scope proficiency", $scope.proficiency);
+				console.log("scope currentClass", $scope.currentClass);
+				console.log("scope currentStudents", $scope.currentStudents);
 			})
 			.catch(error => console.log("error from getAssessment", error.message));
+	};
+
+	const getCurrentClass = (obj, id) => {
+		let currentClass = {};
+		obj.classes.forEach(item => {
+			if (item.id === id) {
+				currentClass = item;
+			}
+		});
+		return currentClass;
+	};
+
+	const getCurrentStudents = (obj, level) => {
+		let currentStudents = [];
+		obj.students.forEach(item => {
+			if (item.proficiency === level) {
+				currentStudents.push(item);
+			}
+		});
+		return currentStudents;
 	};
 
 	//loads assessment data to DOM
