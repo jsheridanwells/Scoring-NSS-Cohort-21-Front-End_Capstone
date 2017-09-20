91a45ec3b98adc7e116e62f3962ee92f1dc5aec7
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-20 15:07:19 -0500

    class list sorting algorithm works

--- app/controllers/assessment-edit-ctrl.js
+++ app/controllers/assessment-edit-ctrl.js
@@ -7,7 +7,8 @@ app.controller('assessmentEditCtrl', function ($scope, $routeParams, assessmentF
 
 	$scope.newAssessmentObj = {};
 	$scope.classes = [];
-	$scope.title = 'EditAssessment';
+	$scope.currentClasses = [];
+	$scope.title = 'Edit Assessment';
 	$scope.buttonText = 'Save Changes';
 
 	const loadAssessmentData = () => {
@@ -15,15 +16,30 @@ app.controller('assessmentEditCtrl', function ($scope, $routeParams, assessmentF
 			.then(assessmentObj => {
 				$scope.newAssessmentObj = assessmentObj;
 				$scope.newAssessmentObj.date = new Date(assessmentObj.date);
-				// new Date(1505797200000).toLocaleDateString();
+				$scope.currentClasses = assessmentObj.classes;
 				console.log("$scope.newAssessmentObj", $scope.newAssessmentObj);
+				console.log("$scope.currentClasses", $scope.currentClasses);
 				return classFactory.getAllClasses(userId);
 			})
 			.then(classes => {
-				$scope.classes = classes;
-				console.log("$scope.classes", $scope.classes);
+				$scope.classes = loadCurrentClasses($scope.currentClasses, classes);
+				console.log("$scope.currentClasses 2", $scope.currentClasses);
+				console.log("$scope.classes 2", $scope.classes);
 			})
 			.catch(error => console.log("error from loadAssessment", error.message));
+	};
+
+	const loadCurrentClasses = (list1, list2) => {
+		for (let i = 0; i < list1.length; i++) {
+			for (let j = 0; j < list2.length; j++) {
+				if (list1[i].id === list2[j].id) {
+					console.log("entering if", list2.indexOf(list2[j]));
+					list2.splice(list2.indexOf(list2[j]), 1);
+				}
+			}
+		}
+		console.log("list2", list2);
+		return list2;
 	};
 
 	loadAssessmentData();
