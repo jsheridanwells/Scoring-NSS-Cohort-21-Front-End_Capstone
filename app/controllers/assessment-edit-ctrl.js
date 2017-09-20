3dcc4bf300cd83ab7bae87803e94ddae501bfd62
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-20 15:21:01 -0500

    adding and removing to lists correctly

--- app/controllers/assessment-edit-ctrl.js
+++ app/controllers/assessment-edit-ctrl.js
@@ -11,37 +11,53 @@ app.controller('assessmentEditCtrl', function ($scope, $routeParams, assessmentF
 	$scope.title = 'Edit Assessment';
 	$scope.buttonText = 'Save Changes';
 
+	//loads data for current assessment, then loads all classes
 	const loadAssessmentData = () => {
 		assessmentFactory.getSingleAssessment($routeParams.assessmentId)
 			.then(assessmentObj => {
 				$scope.newAssessmentObj = assessmentObj;
 				$scope.newAssessmentObj.date = new Date(assessmentObj.date);
 				$scope.currentClasses = assessmentObj.classes;
-				console.log("$scope.newAssessmentObj", $scope.newAssessmentObj);
-				console.log("$scope.currentClasses", $scope.currentClasses);
 				return classFactory.getAllClasses(userId);
 			})
 			.then(classes => {
 				$scope.classes = loadCurrentClasses($scope.currentClasses, classes);
-				console.log("$scope.currentClasses 2", $scope.currentClasses);
-				console.log("$scope.classes 2", $scope.classes);
 			})
 			.catch(error => console.log("error from loadAssessment", error.message));
 	};
 
+	//removes classes already assigned assessment from classes list
 	const loadCurrentClasses = (list1, list2) => {
 		for (let i = 0; i < list1.length; i++) {
 			for (let j = 0; j < list2.length; j++) {
 				if (list1[i].id === list2[j].id) {
-					console.log("entering if", list2.indexOf(list2[j]));
 					list2.splice(list2.indexOf(list2[j]), 1);
 				}
 			}
 		}
-		console.log("list2", list2);
 		return list2;
 	};
 
+	//checks to see if class already exists in classes array
+	const checkClasses = (id) => {
+		for (let i = 0; i < $scope.currentClasses.length; i++) {
+			if (id === $scope.currentClasses[i].id) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	//when user selects a class, appends class information to newAssessmentObj, creates test score field for each student
+	$scope.addRemoveClass = (obj, id) => {
+		if (checkClasses(id)) {
+			$scope.classes.push(obj);
+			$scope.currentClasses.splice($scope.currentClasses.indexOf(obj), 1);
+		} else {
+			$scope.currentClasses.push(obj);
+			$scope.classes.splice($scope.classes.indexOf(obj), 1);
+		}
+	};
 	loadAssessmentData();
 
 });
